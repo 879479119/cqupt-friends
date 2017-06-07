55023d0f5a114db50e8652c2efa534dacf53f760
RockSAMA <767444690@qq.com>
2017-06-07 10:50:29 +0800

    push online

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let Promise = require('bluebird')
 
 /* GET users listing. */
 router.get('/all', function(req, respond, next) {
@@ -14,7 +15,6 @@ router.get('/departList', function(req, respond, next) {
 })
 router.get('/specList', function(req, respond, next) {
 	let p = decodeURI(req.query.depart)
-	console.info(p)
 	conn.query('select * from special where depart = "' +p+ '"', (err, res)=>{
 		respond.send(res)
 	})
@@ -31,5 +31,53 @@ router.get('/fellowList', function(req, respond, next) {
 		respond.send(res)
 	})
 })
+
+/**
+ * change user state
+ */
+
+router.get('/getRandomByClass', function(req, respond, next) {
+	let p = req.query.class
+	let num = req.query.num
+	new Promise((resolve, reject)=>{
+		conn.query('select s_num from student where s_class = "' +p+ '"', (err, res)=>{
+			// respond.send(res)
+			resolve(res)
+		})
+	}).then(res => {
+		if(res.length === 0) throw Error(0)
+		if(res.length < num) throw Error(0)
+		let users = []
+		for(let i = 0;i < num;i ++){
+			users.push(res.splice((Math.random()*res.length) >>> 0, 1)[0]['s_num'])
+		}
+
+		console.info(users)
+
+		return new Promise((resolve, reject)=>{
+			let str = 'insert into random VALUES '
+			let d = new Date().getTime()
+			users.map((item, index)=>{
+				str += `(NULL, ${item}, '${d}'),`
+			})
+			str = str.slice(0,-1)
+			console.info(str)
+			conn.query(str, (err, res)=>{
+				if(err) console.info(err)
+				respond.send({
+					code: 100,
+					data: users
+				})
+			})
+		})
+	}).catch(error=>{
+		respond.send({
+			code: 0,
+			msg: error
+		})
+	})
+})
+
+
 
 module.exports = router
