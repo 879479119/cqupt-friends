de06374af556b7c90d7da7b5a3b5c00f556e588a
RockSAMA <767444690@qq.com>
2017-06-12 23:57:41 +0800

    update: svg loaded

--- webapp/src/components/Class.vue
+++ webapp/src/components/Class.vue
@@ -3,6 +3,16 @@
     <h2>{{cls}}</h2>
     <section>
       <table class="table">
+        <tr>
+          <th>ID</th>
+          <th>学号</th>
+          <th>姓名</th>
+          <th>性别</th>
+          <th>学籍状态</th>
+          <th>被点次数</th>
+          <th>到场次数</th>
+          <th>用户详情</th>
+        </tr>
         <tr v-for="(item, index) in array" :key="index">
           <td>{{item.id}}</td>
           <td>{{item.s_num}}</td>
@@ -10,21 +20,46 @@
           <td>{{item.s_sex}}</td>
           <td>{{item.s_status}}</td>
           <td>{{+ item.r_count}}</td>
+          <td>{{+ item.r_present}}</td>
           <td><router-link :to="'/student/'+item.s_num">详情</router-link></td>
         </tr>
       </table>
+      <percentage :percentages="present"></percentage>
     </section>
   </div>
 </template>
 
 <script>
   import API from "../request"
+  import Percentage from "./Percentage.vue"
   export default {
     name: 'list',
+    components: {
+    	percentage: Percentage
+    },
     data(){
       return {
         cls: '',
         array: []
+      }
+    },
+    computed: {
+    	present(){
+    		let a = 0, b = 0
+    		this.array.map((item, index)=>{
+    			if(item.r_count > item.r_present){
+    				a ++
+          }else{
+    				b ++
+          }
+        })
+        return [{
+    			text: "旷到记录",
+          count: 100 * a / this.array.length
+        },{
+          text: "从未旷到",
+    			count: 100 * b / this.array.length
+        }]
       }
     },
     mounted(){
@@ -45,22 +80,28 @@
   .students{
     background: #f3f3f3;
     table{
+      display: inline-block;
+      tr:nth-child(odd){
+        background: #fff;
 
+      }
       tr{
-        padding: 10px;
-        td{
-          width: 100px;
+        padding: 4px;
+        &:hover{
+          background: #969696;
+        }
+        th,td{
+          max-width: 100px;
+          min-width: 80px;
           margin: 10px;
+          padding: 10px;
           a{
             text-decoration: none;
             color: #00bfff;
           }
         }
       }
-      tr:nth-child(odd){
-        background: #fff;
 
-      }
     }
   }
 </style>
---
+++ webapp/src/components/Percentage.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="percentage">
+    <svg width="400" height="400">
+      <g>
+        <path v-for="(item, index) in paths" :key="index" :d="item.path" stroke-width="40" fill="transparent" :stroke="item.color" :transform="`rotate(${item.rotate})`"></path>
+      </g>
+      <g>
+        <g v-for="(item, index) in paths" :key="index" :transform="`translate(${index*120+100}, 320)`">
+          <rect width="20" height="20" :fill="item.color"></rect>
+          <text fill="#555" x="30" y="16">{{item.text}}</text>
+        </g>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: '',
+		props: ['percentages'],
+		data(){
+			return {
+      }
+		},
+    computed: {
+      paths(){
+      	let sum = 0, colors = ['#00caab', '#00bfff', 'orange', '#efefef']
+
+      	let r = 80, cx = 200, cy = 200
+      	return this.percentages.map((item, index)=>{
+          let progress = item.count / 100
+          let degrees = progress * 360
+          let rad = degrees * (Math.PI / 180)
+          let x = (Math.sin(rad) * r).toFixed(2)
+          let y = -(Math.cos(rad) * r).toFixed(2)
+          let length = window.Number(degrees > 180)
+          let descriptions = ['M', cx, cy - r, 'A', r, r, 0, length, 1, +x + cx, +y + cy]
+          let t = sum
+          sum += degrees
+          return {
+          	text: item.text,
+          	rotate: t,
+          	path: descriptions.join(' '),
+            color: colors[index]
+          }
+        })
+      }
+    }
+	}
+</script>
+
+<style scoped lang="scss">
+  .percentage{
+    display: inline-block;
+    vertical-align: top;
+    path{
+      transform-origin: 200px 200px;
+    }
+  }
+
+</style>
